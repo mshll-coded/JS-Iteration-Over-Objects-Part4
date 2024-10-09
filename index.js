de0363392a0ec1192d4d82df3ed3dc7a1cdfaef3
meshal <imeshalfm@gmail.com>
2024-10-09 15:03:14 +0300

    complete task
    Co-authored-by: Yousef Almesaeed <yousefalm1@users.noreply.github.com>

--- index.js
+++ index.js
@@ -45,9 +45,9 @@ Don't forget to uncomment the console.log
 ===
 ANSWER: Toyota
 **********/
-
+// Yousef
 function getCarMake(car) {
-  // Your code here
+  return car.make;
 }
 // console.log(getCarMake(cars[0]));
 
@@ -60,11 +60,11 @@ You have a function that:
 ===
 ANSWER: false
 **********/
-
+// Meshal
 function isCarColorMatching(car, color) {
-  // Your code here
+  return car.color === color;
 }
-// console.log(isCarColorMatching(cars[1], "Blue"));
+// console.log(isCarColorMatching(cars[1], 'Blue'));
 
 /**********
 Question 3:
@@ -120,17 +120,18 @@ ANSWER:
   }
 ]
 **********/
-
-function addCar(cars, car) {
-  // Your code here
+// Y
+function addCar(cars, newCar) {
+  cars.push(newCar);
+  return cars;
 }
 
 // const newCar = {
-//   "id": 406,
-//   "make": "Kia",
-//   "model": "Sorento",
-//   "year": 2021,
-//   "color": "Green"
+//   id: 406,
+//   make: 'Kia',
+//   model: 'Sorento',
+//   year: 2021,
+//   color: 'Green',
 // };
 
 // console.log(addCar(cars, newCar));
@@ -144,9 +145,9 @@ countCarsMadeInYear(cars, year):
 ===
 ANSWER: 2
 **********/
-
+// Y
 function countCarsMadeInYear(cars, year) {
-  // Your code here
+  return cars.filter((car) => car.year === year).length;
 }
 // console.log(countCarsMadeInYear(cars, 2020));
 
@@ -191,8 +192,9 @@ ANSWER:
   }
 ]
 **********/
+// M
 function removeCarById(cars, id) {
-  // Your code here
+  return cars.filter((car) => car.id !== id);
 }
 // console.log(removeCarById(cars, 404));
 
@@ -214,8 +216,22 @@ ANSWER:
   "color": "Yellow"
 }
 **********/
-
+// Y
 function updateCarColor(cars, id, newColor) {
-  // Your code here
+  const updatedCars = cars.filter((car) => {
+    const foundCar = car.id === id;
+    if (foundCar) car.color = newColor;
+    return foundCar;
+  });
+
+  return updatedCars.length === 1 ? updateCars[0] : 'No Car Found';
 }
-// console.log(updateCarColor(cars, 401, "Yellow"));
+
+// M
+function updateCarColor(cars, id, newColor) {
+  const car = cars.find((car) => car.id === id);
+  if (car) car.color = newColor;
+  return car ?? 'No Car Found';
+}
+
+// console.log(updateCarColor(cars, 401, 'Yellow'));
